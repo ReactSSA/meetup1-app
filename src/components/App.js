77d7957fc2386155598b9b727286e614979eeae0
Luciano Lima <luciano@editorasanar.com.br>
2018-04-05 20:47:15 -0300

    Subindo versão completa do aplicativo

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Header from './Header';
+import List from './List';
+import Loading from './Loading';
 
 const style = {
     container: {
-        alignItems: 'center',
-        flex: 1,
-        justifyContent: 'center'
+        flex: 1
     }
 };
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            books: null
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://demo6669321.mockable.io/livros')
+            .then(response => response.json()
+            .then(({ data }) => this.setState({ books: data })))
+            .catch(error => console.log(error));
+    }
+
     render() {
+        const { books } = this.state;
+
+        if (books === null) {
+            return <Loading />;
+        }
+
         return (
             <View style={style.container}>
-                <Text>Está rodando!</Text>
+                <Header />
+                <List items={this.state.books} />
             </View>
         );
     }
