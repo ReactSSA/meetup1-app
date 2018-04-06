52a74eb009b0172e74bdd2887810678c8f91ba73
Luciano Lima <beavess@gmail.com>
2018-04-06 01:54:28 -0300

    Desabilita os warnings gerados pela nova versão do React

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import App from './src/components/App';
 
+// Desabilita os warnings gerados pela nova versão do React
+// Veja em https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html
+console.disableYellowBox = true;
+
 export default App;
